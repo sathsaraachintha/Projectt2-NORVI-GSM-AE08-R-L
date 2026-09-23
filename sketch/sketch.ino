4cdad7c2999c98106b04f5ef1718fe00c9a74903
sathsaraachintha <230824934+sathsaraachintha@users.noreply.github.com>
2026-09-23 10:31:51 +0000

    Sync to GitHub: 2026-09-23T10:31:50.116Z

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,3 +1,13 @@
-void setup() {}
+#define INPUT1 34 
 
-void loop() {}
+void setup() { 
+   Serial.begin(9600); 
+   Serial.println("Device Starting"); 
+   pinMode(INPUT1, INPUT); 
+} 
+
+void loop() { 
+   Serial.print(digitalRead(INPUT1));
+   Serial.println(""); 
+   delay(500); 
+}
